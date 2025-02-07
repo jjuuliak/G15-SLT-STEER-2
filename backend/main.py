1b9e466eb59e26a85a4b7a280ea44c38dcf44a7c
ndpema <187519691+ndpema@users.noreply.github.com>
2025-02-07 17:14:57 +0200

    Use env file instead of secrets

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,20 @@
 import json
+import os
 
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 
 
 async def connect_mongodb():
-    print("Loading db_password")
-    with open("/run/secrets/db_password", 'r') as file:
-        db_password = file.readline().strip()
-    print(db_password)
+    print("Loading .env")
+    load_dotenv()
+    db_user = os.getenv("MONGO_USER")
+    db_password = os.getenv("MONGO_PASSWORD")
 
     print("Connecting to MongoDB")
     global mongo
-    mongo = AsyncIOMotorClient(f"mongodb://admin:{db_password}@database:27017/")
+    mongo = AsyncIOMotorClient(f"mongodb://{db_user}:{db_password}@database:27017/")
     print(await mongo.server_info())
     return
 
