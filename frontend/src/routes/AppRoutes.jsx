fa3a6ecb89a6ea673d9c1a3d19eb1f3540994a5e
Ville Nousiainen <ville.j.nousiainen@tuni.fi>
2025-02-12 14:30:03 +0200

    Create initial login page

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import LogIn from "../pages/LogIn";
 //import Profile from "../pages/Profile";
 //import Chat from "../pages/Chat";
 //import History from "../pages/History";
 //import Settings from "../pages/Settings";
+//import ForgotPassword from "../pages/ForgotPassword";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LogIn />} />
         {/*<Route path="/data-input" element={<Profile />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/history" element={<History />} />
         <Route path="/settings" element={<Settings />} />*/}
       </Routes>
