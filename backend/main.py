5e681ded4cd2cf005ff9d0bb60a8b890b02787f8
ndpema <187519691+ndpema@users.noreply.github.com>
2025-02-15 23:52:40 +0200

    user auth v1

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,22 @@
-from fastapi import FastAPI, HTTPException
-from motor.motor_asyncio import AsyncIOMotorClient
-from pydantic import BaseModel
-from LLMService import LLMService
-from dotenv import load_dotenv
-import os
 import json
+import os
+import secrets
+import string
+
+# noinspection PyPackageRequirements
+from bson import ObjectId
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Security
+from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer
+from pydantic import BaseModel
+from starlette import status
+from starlette.responses import Response
+
+import database_connection
+from LLMService import LLMService
+from models.user_model import UserModel
 
 load_dotenv()
-
-async def connect_mongodb():
-    db_user = os.getenv("MONGO_USER")
-    db_password = os.getenv("MONGO_PASSWORD")
-
-    print("Connecting to MongoDB")
-    global mongo
-    mongo = AsyncIOMotorClient(f"mongodb://{db_user}:{db_password}@database:27017/")
-    print(await mongo.server_info())
-    return
 
 app = FastAPI()
 
@@ -25,16 +25,22 @@ if not api_key:
     raise ValueError("API_KEY missing from environment variables")
 
 llm_service = LLMService(api_key=api_key)
-app.add_event_handler("startup", connect_mongodb)
+app.add_event_handler("startup", database_connection.connect_mongodb)
+
+
+characters = string.ascii_letters + string.digits + string.punctuation
+access_security = JwtAccessBearer(secret_key="".join(secrets.choice(characters) for _ in range(32)), auto_error=True)
 
 
 @app.get("/")
 async def home():
     return {"message": "Hello from FastAPI!"}
 
+
 class ChatRequest(BaseModel):
     user_id: str
     message: str
+
 
 @app.post("/ask")
 async def ask_llm(request: ChatRequest):
@@ -46,16 +52,76 @@ async def ask_llm(request: ChatRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-def get_mongo():
-    return app.state.mongo
 
 @app.get("/insert")
 async def insert_test():
-    test = await mongo.get_database("test_database").get_collection("test_collection").insert_one({"test": True})
-    created = await mongo.get_database("test_database").get_collection("test_collection").find_one({"_id": test.inserted_id})
-    return {"document": json.dumps( created, default=str)}
+    test = await database_connection.get_test().insert_one({"test": True})
+    created = await database_connection.get_test().find_one({"_id": test.inserted_id})
+    return {"document": json.dumps(created, default=str)}
+
 
 @app.get("/list")
 async def list_test():
-    documents = await mongo.get_database("test_database").get_collection("test_collection").find().to_list(None)
-    return {"documents": json.dumps( [document for document in documents], default=str)}
+    documents = await database_connection.get_test().find().to_list(None)
+    return {"documents": json.dumps([document for document in documents], default=str)}
+
+
+@app.post("/register")
+async def register(user_info: UserModel, response: Response):
+    current_user = await database_connection.get_users().find_one({"email": user_info.email})
+
+    if current_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Account exists")
+
+    add_user = await database_connection.get_users().insert_one(user_info.model_dump(by_alias=True, exclude={"id"}))
+
+    # Prepare profile for userdata and send it (empty user) back as part of the response
+    # TODO: Use a model for this too
+    # TODO: We might want to have an user id as part of the schema instead of relying on mongodb
+    user_id: str = str(add_user.inserted_id)
+    user_data = {"user_id": user_id, "test": True}
+
+    await database_connection.get_user_data().insert_one(user_data)
+
+    access_token = access_security.create_access_token(subject={"user_id": user_id})
+    access_security.set_access_cookie(response, access_token)
+
+    user_data = {key: str(value) if isinstance(value, ObjectId) else value for key, value in user_data.items()}
+
+    return {"status": "success", "access_token": access_token, "user_data": user_data}
+
+
+@app.post("/login")
+async def login(user_info: UserModel, response: Response):
+    existing_user = await database_connection.get_users().find_one({"email": user_info.email})
+
+    if not existing_user or user_info.password != existing_user.get("password"):
+        print("existing_user", existing_user, "user_info.password", user_info.password, "existing_user.get(password)", existing_user.get("password"))
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect login")
+
+    # Get userdata and send it back as part of the response
+    # TODO: Use a model for this too
+    # TODO: Maybe we don't actually want to send back the whole user data especially if it contains long chat histories
+    user_id: str = str(existing_user.get("_id"))
+    user_data = await database_connection.get_user_data().find_one({"user_id": user_id})
+
+    access_token = access_security.create_access_token(subject={"user_id": user_id})
+    access_security.set_access_cookie(response, access_token)
+
+    user_data = {key: str(value) if isinstance(value, ObjectId) else value for key, value in user_data.items()}
+
+    return {"status": "success", "access_token": access_token, "user_data": user_data}
+
+
+@app.post("/logout")
+async def logout(response: Response):
+    # TODO: This does not actually log out, look for better jwt implementation
+    access_security.unset_access_cookie(response)
+    return {"status": "success"}
+
+
+@app.post("/test")
+async def user_test(credentials: JwtAuthorizationCredentials = Security(access_security)):
+    user_data = await database_connection.get_user_data().find_one({"user_id": credentials["user_id"]})
+    user_data = {key: str(value) if isinstance(value, ObjectId) else value for key, value in user_data.items()}
+    return {"user_data": user_data}
--- start.cmd
+++ start.cmd
@@ -1,2 +1,2 @@
-docker compose up --build -d
+docker compose up --build
 pause
