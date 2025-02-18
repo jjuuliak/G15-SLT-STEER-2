eec9f503f8af5bbc009bee526f621c3de6f801b4
Roni Pogrebetski <roni.pogrebetski@tuni.fi>
2025-02-18 13:00:01 +0200

    Split user model into general and login models

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,17 +1,16 @@
 from pydantic import BaseModel, Field, EmailStr, ConfigDict, BeforeValidator
 from typing_extensions import Annotated
 from typing import Optional
-import bcrypt
 
 PyObjectId = Annotated[str, BeforeValidator(str)]
 
-class UserModel(BaseModel):
+class User(BaseModel):
     """
     Model for basic user information
     """
 
     id: Optional[PyObjectId] = Field(alias="_id", default=None, exclude=True)
-    #username: str = Field(...)
+    name: str = Field(...)
     email: EmailStr = Field(...)
     password: str = Field(...)
     model_config = ConfigDict(
@@ -20,29 +19,9 @@ class UserModel(BaseModel):
         extra="forbid",
         json_schema_extra={
             "example": {
-                "username": "Matti Meikäläinen",
+                "name": "matti",
                 "email": "matti.meikalainen@example.com",
-                "password": "verysafepwd123",
+                "password": "verysafepwd123hashed",
             }
         }
     )
-
-    # Utility functions for user-related operations
-    
-    @staticmethod
-    def hash_password(password: str) -> str:
-        """
-        Hashes the given password
-        """
-
-        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode()
-
-    @staticmethod
-    def check_password(password: str, hash: str) -> bool:
-        """
-        Checks if the given password matches the user's stored password hash
-        """
-
-        return bcrypt.checkpw(password.encode("utf-8"), hash.encode("utf-8"))
-
-        
