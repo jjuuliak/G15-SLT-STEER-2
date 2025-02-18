c03cf3f4791edd2517a9be8b2ab07d1e8cc1b2ce
Ville Nousiainen <ville.j.nousiainen@tuni.fi>
2025-02-19 00:08:51 +0200

    update log in view for desktop

--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Box, Typography, InputAdornment, IconButton  } from '@mui/material';
+import { TextField, Button, Box, Typography, InputAdornment, IconButton, useTheme  } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
@@ -10,6 +10,7 @@ const LogIn = () => {
     const [password, setPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
     const [error, setError] = useState('');
+    const theme = useTheme(); // for responsive views
     const navigate = useNavigate();
 
     const loginUser = async () => {
@@ -31,11 +32,158 @@ const LogIn = () => {
       <Box
       sx={{
         display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+        height: '100vh',
+        
+      }}>
+      
+      <Box // Left side green picture
+      sx={{
+        flex: 1,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
+        display: {xs: 'none', md: 'flex'}, // Responsivity
+        backgroundColor: theme.palette.primary.main,
         height: '100vh',
-        padding: 2,
+        position: 'relative',
+        overflow: 'hidden', // Prevent heartbeat svg from overflowing to login view
+      }}>
+    
+        <Box // solita logo
+          sx={{
+            position: 'absolute',
+            top: '20px',
+            left: '20px',
+            width: '50px',
+          }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="64"
+            height="80"
+            viewBox="0 0 64 80"
+            fill="none"
+          >
+            <rect y="40" width="20" height="40" fill="white" />
+            <rect x="44" width="20" height="40" fill="white" />
+            <rect x="22" width="20" height="80" fill="white" />
+          </svg>
+      </Box>
+
+      
+
+
+      <Box // Box for Text and heartbeat
+      sx={{
+        marginRight: 13,
+        position: "relative"
+      }}>
+
+    <Box // Box for heartbeat svg
+      sx={{
+        position: "absolute",
+        top: '16%',
+        left: '70px',
+        width: '90%',
+        opacity: 0.6,
+      
+      }}
+    >
+      <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="460"
+  height="144"
+  viewBox="0 0 460 144"
+  fill="none"
+>
+  <g filter="url(#filter0_d_121_346)">
+    <path
+      d="M4 79.5H216.5L224 69.5L233 79.5H238.5L240 83L241.5 79.5L263 1L269 135L287.5 69.5L294.5 79.5H462.5"
+      stroke="url(#paint0_linear_121_346)"
+      strokeWidth="2"
+      shapeRendering="crispEdges"
+    />
+  </g>
+  <defs>
+    <filter
+      id="filter0_d_121_346"
+      x="0"
+      y="0.73584"
+      width="466.5"
+      height="142.536"
+      filterUnits="userSpaceOnUse"
+      colorInterpolationFilters="sRGB"
+    >
+      <feFlood floodOpacity="0" result="BackgroundImageFix" />
+      <feColorMatrix
+        in="SourceAlpha"
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        result="hardAlpha"
+      />
+      <feOffset dy="4" />
+      <feGaussianBlur stdDeviation="2" />
+      <feComposite in2="hardAlpha" operator="out" />
+      <feColorMatrix
+        type="matrix"
+        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+      />
+      <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_121_346" />
+      <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_121_346" result="shape" />
+    </filter>
+    <linearGradient
+      id="paint0_linear_121_346"
+      x1="4"
+      y1="68"
+      x2="462.5"
+      y2="68"
+      gradientUnits="userSpaceOnUse"
+    >
+      <stop stopColor="white" stopOpacity="0" />
+      <stop offset="0.3" stopColor="white" />
+      <stop offset="0.7" stopColor="white" />
+      <stop offset="1" stopColor="white" stopOpacity="0" />
+    </linearGradient>
+  </defs>
+</svg>
+
+    </Box>
+      <Typography
+      sx={{
+        background: 'linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.44) 100%)',
+        backgroundClip: 'text',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+        fontSize: '4.5rem',
+        fontWeight: 500,
+      }}>
+        LIFELINE
+      </Typography>
+      
+      <Typography
+      sx={{
+        background: 'linear-gradient(180deg, #FFF 0%, rgba(255, 255, 255, 0.44) 100%)',
+        backgroundClip: 'text',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
+        fontSize: '3rem',
+        fontWeight: 500,
+      }}>
+        For your heart.
+      </Typography>
+      </Box>
+      </Box>
+
+
+      <Box // Box for login view
+      sx={{
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
         backgroundColor: 'white',
       }}
     >
@@ -97,23 +245,20 @@ const LogIn = () => {
 
           <Box sx={{ marginTop: 10, display: 'flex', justifyContent: 'center' }}>
             <Typography 
-            sx={{marginRight: 1.5, padding: 1, fontWeight: "bold", textTransform: "none", fontSize: "1.1rem"}}
+            sx={{marginRight: 1.3, padding: 1, fontWeight: "bold", textTransform: "none", fontSize: "1.1rem"}}
             variant="button">
                 No account?
             </Typography>
           <Button
            onClick={() => navigate('/register')}
-          sx={{marginLeft: 1.5, borderRadius: 1.5,fontWeight: "bolder", paddingX: 2.5, textTransform: "none"}}
+          sx={{marginLeft: 1.3, borderRadius: 1.5,fontWeight: "bolder", paddingX: 2.5, textTransform: "none"}}
           variant="outlined">Sign up here</Button> 
           </Box>
 
           </Box>
+         </Box>
         </Box>
     );
 }
  
 export default LogIn;
-
-
-
-
