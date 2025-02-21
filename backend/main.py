51d432914c20d95994ed3781d8f6affa0e4e1806
ndpema <187519691+ndpema@users.noreply.github.com>
2025-02-21 19:07:49 +0200

    Remove old test code and clean up backend
    /ask no longer takes user_id but expects access_token instead

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,15 @@
-import os
+import logging
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from requests import Request
+from starlette import status
+from starlette.responses import JSONResponse
 
 import database_connection
-from LLMService import LLMService
-from routes import database_test
-from routes import login
+from routes import login, chat
 
 
 load_dotenv()
@@ -16,7 +17,7 @@ load_dotenv()
 
 app = FastAPI()
 app.add_event_handler("startup", database_connection.connect_mongodb)
-app.include_router(database_test.router)
+app.include_router(chat.router)
 app.include_router(login.router)
 app.add_middleware(
     CORSMiddleware,
@@ -26,30 +27,15 @@ app.add_middleware(
     allow_headers=["*"])  # TODO
 
 
-api_key = os.getenv("API_KEY")
-if not api_key:
-    raise ValueError("API_KEY missing from environment variables")
-
-
-llm_service = LLMService(api_key=api_key)
+# Test only: print more details on schema validation errors
+logger = logging.getLogger(__name__)
+@app.exception_handler(RequestValidationError)
+async def validation_error_handler(request: Request, exc: RequestValidationError):
+    logger.error(f"Validation error for: {await request.json()}")
+    logger.error(f"Errors: {exc.errors()}")
+    raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Schema validation failed")
 
 
 @app.get("/")
 async def home():
     return {"message": "Hello from FastAPI!"}
-
-
-class ChatRequest(BaseModel):
-    user_id: str
-    message: str
-
-
-@app.post("/ask")
-async def ask_llm(request: ChatRequest):
-    if not request.message.strip():
-        raise HTTPException(status_code=400, detail="Message cannot be empty.")
-    try:
-        response_text = llm_service.send_message(request.user_id, request.message)
-        return {"response": response_text}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
