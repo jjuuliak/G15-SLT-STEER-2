2e94e498ce85cf29509f9fc32a30e6a15d8f15f9
ndpema <187519691+ndpema@users.noreply.github.com>
2025-03-01 21:43:59 +0200

    Don't replace existing values with null

--- backend/routes/user.py
+++ backend/routes/user.py
@@ -11,8 +11,10 @@ router = APIRouter()
 
 @router.post("/update-profile")
 async def update_user_profile(request: MedicalInfo, credentials: JwtAuthorizationCredentials = Security(AuthService.get_access_security())):
-    result = await database_connection.get_user_data().update_one({"user_id": credentials["user_id"]},
-                                                            {"$set": request.model_dump(exclude={"id"})})
+    # Don't replace existing values with null if not present
+    update_data = {"$set": {k: v for k, v in request.model_dump(exclude_unset=True, exclude={"id"}).items() if v is not None}}
+
+    result = await database_connection.get_user_data().update_one({"user_id": credentials["user_id"]}, update_data)
 
     if not result or not result.modified_count == 1:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="User data update failed")
