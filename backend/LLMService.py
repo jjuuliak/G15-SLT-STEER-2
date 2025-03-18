2832971bee43781b6733590fdb74be838a6e9631
Roni Pogrebetski <roni.pogrebetski@tuni.fi>
2025-03-18 02:11:02 +0200

    Add AI response streaming

--- backend/LLMService.py
+++ backend/LLMService.py
@@ -1,10 +1,11 @@
 import inspect
-
 import google.generativeai as genai
 from typing import Dict
 from rag_service import RAGService
 import chat_history
 import message_attributes
+from typing import AsyncGenerator
+import json
 
 rag = RAGService()
 
@@ -42,45 +43,44 @@ class LLMService:
                 tools=[function for name, function in inspect.getmembers(message_attributes) if inspect.isfunction(function)],
                 tool_config={"function_calling_config": {"mode": "auto"}}
             )
-            self.sessions[user_id] = model.start_chat(history=await chat_history.load_history(user_id))
+            self.sessions[user_id] = model.start_chat()
         return self.sessions[user_id]
 
 
-    async def send_message(self, user_id: str, message: str) -> {}:
+    async def send_message(self, user_id: str, message: str) -> AsyncGenerator[dict, None]:
         """
-        Asks question from AI model and returns the answer + possible attributes
+        Asks question from AI model and returns the streamed answer and possible attributes
         """
         session = await self.get_session(user_id)
         rag_prompt = rag.build_prompt(message)
-        response = session.send_message(rag_prompt)
 
-        # If response contains function calls, return them as attributes
-        if response and len(response.candidates[0].content.parts) > 1:
-            attributes = []
+        try:
+            response = session.send_message(rag_prompt, stream=True)
+        except Exception:
+            yield json.dumps({"response": "Error: No response from model."})
+            return
 
-            for part in response.candidates[0].content.parts[1:]:
-                attribute = {}
-                for key, value in part.function_call.args.items():
-                    attribute[key] = value
-                attributes.append(attribute)
+        full_answer = ""
+        attributes = []
 
-            answer = response.candidates[0].content.parts[0].text
+        for chunk in response:
+            for candidate in chunk.candidates:
+                for part in candidate.content.parts:
+                    # Save and send text chunk by chunk if present
+                    if 'text' in part:
+                        full_answer += part.text
+                        yield json.dumps({"response": part.text})
 
-            if not answer.strip():
-                return {"response": "Error: No response from model."}
+                    # Save function call attributes and values if present
+                    if 'function_call' in part:
+                        # Function call always has one pair so take first
+                        key, value = list(part.function_call.args.items())[0]
+                        attributes.append({key: value})
+        
+        if attributes:
+            yield json.dumps({"attributes": attributes})
 
-            chat_history.store_history(user_id, message, answer)
-
-            return {"response": answer, "attributes": attributes}
-
-        elif response and response.text:
-            chat_history.store_history(user_id, message, response.text)
-
-            return {"response": response.text}
-
-        else:
-            return {"response": "Error: No response from model."}
-
+        chat_history.store_history(user_id, message, full_answer)
 
     def correct_prompt(self, prompt: str) -> str:
         """
