08a4aed8d3a7d613b2d099769cb922e69f93cad8
Ville Nousiainen <villenousiainen2@gmail.com>
2025-04-09 11:17:47 +0300

    Import some hamster pics and implement mapping from stats to pic

--- frontend/src/pages/HamsterCollection.jsx
+++ frontend/src/pages/HamsterCollection.jsx
@@ -1,19 +1,84 @@
 import TopBar from "../components/TopBar";
 import { Container, Typography, Box, Grid2 as Grid , LinearProgress, Paper} from "@mui/material";
-import doctorBronze from "../images/hamsters/doctor-bronze.png"
-import emptyHamster from "../images/hamsters/empty-hamster.png"
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
+//pic imports
+import emptyHamster from "../images/hamsters/empty-hamster.png"
+
+//messages
+import doctorBronze from "../images/hamsters/doctor-bronze.png"
+
+import doctorSilver from "../images/hamsters/workoutSilver.png" // Change to actual picture, this is for testing
+import doctorGold from "../images/hamsters/workoutSilver.png" // Change to actual picture, this is for testing
+
+import doctorDiamond from "../images/hamsters/doctor-diamond.png"
+
+//meal plans
+import mealPlanBronze from "../images/hamsters/mealBronze.png" // Same as mealbronze for now
+
+//meals
+import mealBronze from "../images/hamsters/mealBronze.png"
+import mealSilver from "../images/hamsters/mealSilver.png"
+import mealGold from "../images/hamsters/mealGold.png"
+
+
+//workout plans
+import workoutPlanBronze from "../images/hamsters/workoutPlanBronze.png"
+
+//workouts
+import workoutBronze from "../images/hamsters/workoutBronze.png"
+import workoutSilver from "../images/hamsters/workoutSilver.png"
+import workoutGold from "../images/hamsters/workoutGold.png"
+
+const hamsterImageMap = {
+    messages: {
+        0: emptyHamster,
+        1: doctorBronze,
+        2: doctorSilver,
+        3: doctorGold,
+        4: doctorDiamond
+    },
+    meal_plans: {
+        0: emptyHamster,
+        1: mealPlanBronze
+    },
+    meal: {
+        0: emptyHamster,
+        1: mealBronze,
+        2: mealSilver,
+        3: mealGold
+    },
+    workout_plans: {
+        0: emptyHamster,
+        1: workoutPlanBronze
+    },
+    workouts: {
+        0: emptyHamster,
+        1: workoutBronze,
+        2: workoutSilver,
+        3: workoutGold
+    }
+  };
+
+  const levelLabels = {
+    0: "Hidden",
+    1: "Bronze",
+    2: "Silver",
+    3: "Gold",
+    4: "Diamond"
+  };
 
 
 const HamsterCollection = () => {
     const accessToken = useSelector((state) => state.auth?.access_token);
     const [currentStat, setCurrentStat] = useState(null);
     const [nextLevel, setNextLevel] = useState(null);
+    const [userStats, setUserStats] = useState([]);
 
      const fillHamsters = (data) => {
-        console.log(data.user_stats)
+        
+        setUserStats(data.user_stats);
 
         data.user_stats.map((category) => {
             if (category.level > 0) {
@@ -22,6 +87,7 @@ const HamsterCollection = () => {
             if (category.stat === 'messages'){
                 setCurrentStat(category.counter);
                 setNextLevel(category.next_level);
+
             }
         })
       };
@@ -94,20 +160,36 @@ const HamsterCollection = () => {
                 <Grid size={{xs: 12, md: 7}}>
                     <Paper elevation={2} sx={{marginX: 2}}>
                     <Grid container spacing={1} >
-                    {hamsters.map((item) => (
-                            <Grid size={{xs: 6, md: 4}}
-                            sx={{marginX: 'auto'}}
-                            key={item.title}
+                    {hamsters.map((item) => {
+                        const userStat = userStats.find(stat => stat.stat === item.stat);
+                        const level = userStat?.level || 0;
+                        
+                        const image = hamsterImageMap[item.stat]?.[level] || emptyHamster;
+
+                        return (
+                            <Grid 
+                                size={{ xs: 6, md: 4 }}
+                                sx={{ marginX: 'auto',
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    justifyContent: 'space-between',
+                                    alignItems: 'center',}}
+                                    key={item.title}
                             >
-                            <img
+                                <img
+                                    src={image}
+                                    alt={`${item.title} level ${level}`}
+                                    loading="lazy"
+                                    style={{ width: "100%", height: "100%", objectFit: "contain", maxWidth: 250 }}
+                                />
+                                <Box sx={{ textAlign: "center"}}>
+                                    <Typography variant="h5">{item.title}</Typography>
+                                    <Typography variant="subtitle1">{levelLabels[level]}</Typography>
+                                </Box>
                                 
-                                src={item.img}
-                                alt={item.title}
-                                loading="lazy"
-                                style={{width: "100%", height: "auto", maxHeight: 250, maxWidth: 250 }} 
-                            />
                             </Grid>
-                        ))}
+                        );
+                    })}
                     </Grid>
 
                     </Paper>
