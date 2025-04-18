a914be64d58d32a0a0c060312b9b29227a461c3f
ndpema <187519691+ndpema@users.noreply.github.com>
2025-04-18 13:27:31 +0300

    Add clear history button to settings and call /clear-history with it

--- frontend/src/components/popup/views/GeneralView.jsx
+++ frontend/src/components/popup/views/GeneralView.jsx
@@ -1,9 +1,46 @@
-
-
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Button, Box, Typography } from "@mui/material";
+import { useTranslation } from 'react-i18next';
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from 'react-router';
+import { fetchWithAuth } from '../../../services/authService';
 
 const GeneralView = () => {
-  return <Typography variant="body1">General settings content</Typography>;
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const accessToken = useSelector((state) => state.auth?.access_token);
+  const refreshToken = useSelector((state) => state.auth?.refresh_token);
+
+  const [cleared, setCleared] = useState(false);
+
+  const clearHistory = () => {
+    if (cleared) {
+      return;
+    }
+
+    setCleared(true);
+
+    fetchWithAuth("http://localhost:8000/clear-history", {
+      method: "POST",
+      headers: null // Default set in fetchWithAuth
+    }, accessToken, refreshToken, dispatch, navigate);
+  };
+
+  return (
+    <Box>
+      <Typography sx={{margin: 2}}> {t("generalSettings")} </Typography>
+      <Box sx={{ display: "flex", gap: 2, p: 2 }}> 
+      <Button 
+        variant={cleared ? "outlined" : "contained"} 
+        onClick={() => clearHistory()}
+      >
+        {cleared ? t("ClearHistoryButtonClicked") : t("ClearHistoryButton")}
+      </Button>
+    </Box>
+    </Box>
+  );
 };
 
 export default GeneralView;
